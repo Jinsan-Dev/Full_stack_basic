5d289664551c96ef994b4e6a2e6d352e59aed9cf
Jinsan-Dev <jinsan.dev@gmail.com>
2021-08-08 20:33:29 +0900

    Add project

---
+++ Full_stack_basic/src/main/java/com/js_example/Full_stack_basic/FullStackBasicApplication.java
@@ -0,0 +1,13 @@
+package com.js_example.Full_stack_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackBasicApplication.class, args);
+	}
+
+}
---
+++ Full_stack_basic/src/test/java/com/js_example/Full_stack_basic/FullStackBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js_example.Full_stack_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
